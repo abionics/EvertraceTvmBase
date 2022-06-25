e42a760508dcf54a58163213f8d385d09a0efbdd
Abionics <abionics.dev@gmail.com>
2022-06-25 15:47:14 +0300

    Add idx checking

--- tvmbase/utils/tvm_utils.py
+++ tvmbase/utils/tvm_utils.py
@@ -1,10 +1,14 @@
 import re
 
-from tvmbase.constants import ADDRESS_REGEX
+from tvmbase.constants import ADDRESS_REGEX, IDX_REGEX
 
 
-def is_address(string: str) -> bool:
-    return bool(re.fullmatch(ADDRESS_REGEX, string))
+def is_address(value: str) -> bool:
+    return bool(re.fullmatch(ADDRESS_REGEX, value))
+
+
+def is_idx(value: str) -> bool:
+    return bool(re.fullmatch(IDX_REGEX, value))
 
 
 def decode_bytes(value: str) -> str:
